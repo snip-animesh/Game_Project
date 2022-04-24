4a9f57df177722af8ebd16f5a86cf7f15a778f06
snip-animesh <animesh12126@gmail.com>
2022-04-24 20:31:07 +0600

    puddle created in pong main.py file

--- Pong/main.py
+++ Pong/main.py
@@ -2,25 +2,48 @@ import pygame
 
 pygame.init()
 WIDTH, HEIGHT = 700, 500
+PADDLE_HEIGHT, PADDLE_WIDTH = 100, 20
+
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("PONG")
+
 FPS = 60
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 
 
-def draw(win):
+class Paddle:
+    COLOR = WHITE
+
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+
+    def draw(self, win):
+        pygame.draw.rect(win, self.COLOR, (self.x, self.y, self.width, self.height))
+
+
+def draw(win, paddles):
     win.fill(BLACK)
+
+    for paddle in paddles:
+        paddle.draw(win)
+
     pygame.display.update()
 
 
 def main():
     run = True
     clock = pygame.time.Clock()
+    left_paddle = Paddle(10, HEIGHT // 2 - PADDLE_HEIGHT // 2, PADDLE_WIDTH, PADDLE_HEIGHT)
+    right_paddle = Paddle(WIDTH - 10 - PADDLE_WIDTH, HEIGHT // 2 - PADDLE_HEIGHT // 2, PADDLE_WIDTH, PADDLE_HEIGHT)
 
     while run:
         clock.tick(FPS)
-        draw(WIN)
+        draw(WIN, [left_paddle, right_paddle])
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
