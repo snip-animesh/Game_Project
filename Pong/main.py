0d7ed219927af2dc826ad3efa30d75cca9c1f279
snip-animesh <animesh12126@gmail.com>
2022-04-24 12:26:47 +0600

    pong upto pygame setup

--- Pong/main.py
+++ Pong/main.py
@@ -1,4 +1,32 @@
 import pygame
 
-WIDTH,HEIGHT=700,500
-WIN=pygame.display.set_mode((WIDTH,HEIGHT))
+pygame.init()
+WIDTH, HEIGHT = 700, 500
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("PONG")
+FPS = 60
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+
+def draw(win):
+    win.fill(BLACK)
+    pygame.display.update()
+
+
+def main():
+    run = True
+    clock = pygame.time.Clock()
+
+    while run:
+        clock.tick(FPS)
+        draw(WIN)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+    pygame.quit()
+
+
+if __name__ == "__main__":
+    main()
